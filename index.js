7f3970b6a2e3370304af21407e229b7b5fee1e2d
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-07-21 23:53:26 -0300

    añadi eventos de javascript que vuelven a mi pagina aun mas dinamica

--- index.js
+++ index.js
@@ -15,43 +15,41 @@ const reclamos =[
     "Para iniciar el reclamo, contactese a reclamos@fincatierras.com"
 ]
 
-let seccion = prompt("¡Hola, Bienvenido a Finca Tierras Catamarqueñas! Desea comprar, contactar o reclamar?");
-/* let imgContainer = document.getElementById("imagenContainer");
+/* let seccion = prompt("¡Hola, Bienvenido a Finca Tierras Catamarqueñas! Desea comprar, contactar o reclamar?"); */
 
-if(eleccion === "comprar"){
-    let producto = prompt("Tenemos aceite, nueces y vinos. Que desea comprar?");
-        if(producto === "aceite"){
-            alert(productos[0]);
-        }else if(producto === "nueces"){
-            alert(productos[1]);;
-        }else if(producto = "vino"){
-            alert(productos[2]);;
-        }else{
-            alert("Producto no encontrado");
-        }
-}else if(eleccion = "contactar"){
-    alert(contactos);
-}else if(eleccion = "reclamo"){
-    alert(reclamos);
-}  
-if(producto === "aceite"){
-    imagenContainer.className = "imgcontainergreen";
-};
- */
-let bienvenida = document.getElementById("bienvenida");
+let botonProductos = document.getElementById("seleccionProducto");
+let botonContacto = document.getElementById("seleccionContacto");
+let botonReclamos = document.getElementById("seleccionReclamos");
+let dynamicText = document.getElementById("dynamicText");
+
+botonProductos.addEventListener("mousedown", () => {
+    dynamicText.innerHTML = "<h4>Tenemos Aceite de Oliva Extra Virgen, Nueces Californianas y Vinos</h4>";
+});
+botonContacto.addEventListener("mousedown", () => {
+    dynamicText.innerHTML = "<h4>Podes seguirnos en Instagram: @fincatierras, Facebook.com/FincaTierrasC o contactarnos en info@fincatierras.com.ar</h4>";
+});
+botonReclamos.addEventListener("mousedown", () => {
+    dynamicText.innerHTML = "<h4>Para iniciar un reclamo o despejar una duda, contactanos por mail en info@fincatierras.com.ar con el asunto RECLAMO URGENTE</h4>";
+});
+
+
+
+
+/* let bienvenida = document.getElementById("bienvenida");
 if(seccion === "comprar"){
     bienvenida.innerHTML = "<h4>Tenemos aceite de oliva extra virgen y nueces californianas de la mas alta calidad</h4>";
-    bienvenida.className = "h4green";
+    bienvenida.classList.add('h4green');
 }else if(seccion === "contactar"){
     bienvenida.innerHTML = "<h4>Podes contactarnos en instagram @fincatierras, facebook.com/fincatierrasc o nuestra tienda en mercadolibre</h4>";
-    bienvenida.className = "h4black";
+    bienvenida.classList.add('h4black');
 }else if(seccion === "reclamar"){
     bienvenida.innerHTML = "<h4>Para reclamos, devoluciones o sugerencias, por favor contactanos en info@fincatierras.com.ar con el asunto reclamo</h4>";
-    bienvenida.className = "h4"
+    bienvenida.classList.add('h4');
 }else {
     bienvenida.innerHTML = "<h4>Bienvenido, que estas buscando?</h4>";
+    bienvenida.classList.add('h4black');
 }
-
+ */
 
 
 
